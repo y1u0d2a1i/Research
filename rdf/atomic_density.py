47d0509654147a854168faa677d3054e6361ac1e
y1u0d2a1i <y1u0d2a1i@icloud.com>
2021-10-31 15:00:42 +0900

    density done

--- rdf/atomic_density.py
+++ rdf/atomic_density.py
@@ -57,47 +57,38 @@ class AtomicDensity:
                 n_center_atom += 1
         return n_center_atom
 
-    def calc_density(self, structure, center_elm, another_elm, r_min, r_max, bins, NA):
-        all_density_list = []
+    def calc_density(self, structure, center_elm, another_elm, r_max):
         center_elm = Element(center_elm)
         another_elm = Element(another_elm)
-        dr = (r_max - r_min) / bins
         n_center_atom = self.get_num_of_center_atom(structure, center_elm)
-        for i in range(bins):
-            density_list = []
-            r_cut = r_min + (dr * i)
-            for s in structure:
-                if (s.specie.value == center_elm.value):
-                    nn = structure.get_neighbors(s, r_cut)
-                    # print("center", s.coords, s.specie)
-                    natom = 0
-                    for n in nn:
-                        if (n[0].specie.value == another_elm.value):
-                            natom += 1
-                            # print("cart", n[0].coords, n[0].specie, "distance", n[1])
-                    vol_sphere = 4 / 3 * np.pi * r_cut ** 3
-                    mass_atom = another_elm.atomic_mass * (natom / NA)
-                    density = mass_atom / vol_sphere
-                    density_list.append(density)
-            density_list = np.array(density_list)
-            density_average = np.average(density_list) / n_center_atom
-            all_density_list.append(density_average)
-        all_density_list = np.array(all_density_list)
-        return np.average(all_density_list), n_center_atom
+        density_list = []
+        for s in structure:
+            if (s.specie.value == center_elm.value):
+                nn = structure.get_neighbors(s, r_max)
+                # print("center", s.coords, s.specie)
+                natom = 0
+                for n in nn:
+                    if (n[0].specie.value == another_elm.value):
+                        natom += 1
+                        # print("cart", n[0].coords, n[0].specie, "distance", n[1])
+                vol_sphere = 4 / 3 * np.pi * r_max ** 3
+                density = natom / vol_sphere
+                density_list.append(density)
+        density_list = np.array(density_list)
+        # density_average = np.average(density_list) / n_center_atom
+        return np.average(density_list), n_center_atom
 
     def run_this_func(self):
-        finished = [
-            'alpha-quartz',
-            'beta-trydymite',
-            'Fdd2-beta-critobalite',
-        ]
         PATH = [
             'alpha-critobalite',
+            'alpha-quartz',
             'beta-quartz',
+            'beta-trydymite',
+            'coesite',
+            'Fdd2-beta-critobalite',
+            'hex-trydymite',
+            'stishovite'
         ]
-        # アボガドロ数
-        # NA = 6.02214085774 * 10 ** 23
-        NA = 6.02
         dirs = glob.glob(f'{self.xsf_file_path}/data*')
 
         for directory in dirs:
@@ -121,7 +112,7 @@ class AtomicDensity:
                         writer.writerow(
                             ['structure_idx', 'name_of_structure', 'num center atom', 'density', 'center atom',
                              'another atom'])
-                    density, n_center_atom = self.calc_density(structure, 'Si', 'Si', 1, 15, 70, NA)
+                    density, n_center_atom = self.calc_density(structure, 'Si', 'Si', 15)
                     print(density,n_center_atom)
                     writer.writerow([structure_idx,name_of_structure,n_center_atom,density,'Si','Si'])
 
@@ -131,7 +122,7 @@ class AtomicDensity:
                         writer.writerow(
                             ['structure_idx', 'name_of_structure', 'num center atom', 'density', 'center atom',
                              'another atom'])
-                    density, n_center_atom = self.calc_density(structure, 'O', 'Si', 1, 15, 70, NA)
+                    density, n_center_atom = self.calc_density(structure, 'O', 'Si', 15)
                     print(density,n_center_atom)
                     writer.writerow([structure_idx,name_of_structure,n_center_atom,density,'O','Si'])
 
@@ -141,7 +132,7 @@ class AtomicDensity:
                         writer.writerow(
                             ['structure_idx', 'name_of_structure', 'num center atom', 'density', 'center atom',
                              'another atom'])
-                    density, n_center_atom = self.calc_density(structure, 'Si', 'O', 1, 15, 70, NA)
+                    density, n_center_atom = self.calc_density(structure, 'Si', 'O', 15)
                     print(density,n_center_atom)
                     writer.writerow([structure_idx,name_of_structure,n_center_atom,density,'Si','O'])
 
@@ -151,11 +142,11 @@ class AtomicDensity:
                         writer.writerow(
                             ['structure_idx', 'name_of_structure', 'num center atom', 'density', 'center atom',
                              'another atom'])
-                    density, n_center_atom = self.calc_density(structure, 'O', 'O', 1, 15, 70, NA)
+                    density, n_center_atom = self.calc_density(structure, 'O', 'O', 15)
                     print(density,n_center_atom)
                     writer.writerow([structure_idx,name_of_structure,n_center_atom,density,'O','O'])
 
 
-atomic_density = AtomicDensity('/home/y1u0d2/Program/python/RDF/density','/home/y1u0d2/Program/python/RDF/xsf')
+atomic_density = AtomicDensity('/Users/y1u0d2/desktop/Lab/Program/python/RDF/20211031/density','/Users/y1u0d2/desktop/Lab/Program/python/RDF/xsf')
 atomic_density.run_this_func()
 
