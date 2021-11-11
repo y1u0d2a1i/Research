e59f78127abf59ec3d9b5e107caa2e5082717120
y1u0d2a1i <y1u0d2a1i@icloud.com>
2021-11-11 12:01:12 +0900

    定数クラス作成

---
+++ utils/constants/dir_path.py
@@ -0,0 +1,8 @@
+class DataDirPath:
+    @staticmethod
+    def xsf():
+        return '/Users/y1u0d2/desktop/Lab/data/xsf'
+
+    @staticmethod
+    def qmas_data():
+        return '/Users/y1u0d2/desktop/Lab/data/qmas_data'
