174f1e1949af0df406c19234ab15baf69a9c2c18
y1u0d2a1i <y1u0d2a1i@icloud.com>
2021-11-22 22:49:04 +0900

    trainingflow作成

--- n2p2/analyze_flow.py
+++ n2p2/analyze_flow.py
@@ -15,7 +15,11 @@ class N2p2AnalyzeFlow():
 
     def make_analyze_dir(self):
         path = f'{self.dir_path}/analyze'
-        if not os.path.exists(path):
+        if os.path.exists(path):
+            files = glob.glob(f'{path}/*.csv')
+            for file in files:
+                os.remove(file)
+        else:
             os.mkdir(path)
 
     def convert_test_train_csv(self):
@@ -24,33 +28,23 @@ class N2p2AnalyzeFlow():
         if test_files and train_files:
             obj = ConvertCsv(output_dir=f'{self.dir_path}/analyze', target_dir=self.dir_path)
             for test_file_path, train_file_path in zip(test_files, train_files):
-                test_file = pd.read_csv(test_file_path)
-                train_file = pd.read_csv(train_file_path)
                 out_test_file_name = test_file_path.split('/')[-1]
                 out_train_file_name = train_file_path.split('/')[-1]
                 # csvファイルに変換
                 obj.convert_n2p2_output_to_csv(10, out_test_file_name, f'{out_test_file_name}')
                 obj.convert_n2p2_output_to_csv(10, out_train_file_name, f'{out_train_file_name}')
 
-    def get_test_score(self, df_test_new):
-        # テストに対して
-        mae_test = mean_absolute_error(df_test_new['Eref'], df_test_new['Ennp'])
-        mse_test = mean_squared_error(df_test_new['Eref'], df_test_new['Ennp'])
-        rmse_test = np.sqrt(mse_test)
-        r2_test = r2_score(df_test_new['Eref'], df_test_new['Ennp'])
-        print('TEST')
-        print(f'mae:{mae_test}, mse:{mse_test}, rmse:{rmse_test}, r2:{r2_test}')
-        return ['test', mae_test, mse_test, rmse_test, r2_test]
-
-    def get_train_score(self, df_train_new):
-        # 訓練データに対して
-        mae_train = mean_absolute_error(df_train_new['Eref'], df_train_new['Ennp'])
-        mse_train = mean_squared_error(df_train_new['Eref'], df_train_new['Ennp'])
-        rmse_train = np.sqrt(mse_train)
-        r2_train = r2_score(df_train_new['Eref'], df_train_new['Ennp'])
-        print('TRAIN')
-        print(f'mae:{mae_train}, mse:{mse_train}, rmse:{rmse_train}, r2:{r2_train}')
-        return ['train', mae_train, mse_train, rmse_train, r2_train]
+    @staticmethod
+    def get_score(df, type):
+        try:
+            mae = mean_absolute_error(df['Eref'], df['Ennp'])
+            mse = mean_squared_error(df['Eref'], df['Ennp'])
+            rmse = np.sqrt(mse)
+            r2 = r2_score(df['Eref'], df['Ennp'])
+        except TypeError as e:
+            print('NANが含まれています')
+            return
+        return [type, mae, mse, rmse, r2]
 
     def write_score_csv(self, epoch, test_score, train_score):
         test_score.insert(0, epoch)
@@ -62,23 +56,30 @@ class N2p2AnalyzeFlow():
             writer.writerow(test_score)
             writer.writerow(train_score)
 
-
 if __name__ == '__main__':
-    dir_path = '/Users/y1u0d2/Desktop/Lab/result/nnp-train/two-body/for_angular/nnp-train_10_10'
-    analyze_flow = N2p2AnalyzeFlow(dir_path)
-    analyze_flow.make_analyze_dir()
-    analyze_flow.convert_test_train_csv()
-    base_df = get_reindex_base()
-    all_test_csv = glob.glob(f'{dir_path}/analyze/testpoints*')
-    all_train_csv = glob.glob(f'{dir_path}/analyze/trainpoints*')
-    all_test_csv.sort()
-    all_train_csv.sort()
-    for i, (test_csv, train_csv) in enumerate(zip(all_test_csv, all_train_csv)):
-        epoch = i
-        df_test = pd.read_csv(test_csv)
-        df_train = pd.read_csv(train_csv)
-        df_test_new = pd.merge(df_test, base_df, left_on='index', right_on='structure_idx')
-        df_train_new = pd.merge(df_train, base_df, left_on='index', right_on='structure_idx')
-        test_score = analyze_flow.get_test_score(df_test_new)
-        train_score = analyze_flow.get_train_score(df_train_new)
-        analyze_flow.write_score_csv(epoch, test_score, train_score)
+    dir_path = '/Users/y1u0d2/Desktop/Lab/result/nnp-train/rdf-inform/20211117'
+    def conduct_flow(dir_path):
+        analyze_flow = N2p2AnalyzeFlow(dir_path)
+        analyze_flow.make_analyze_dir()
+        analyze_flow.convert_test_train_csv()
+        base_df = get_reindex_base()
+        all_test_csv = glob.glob(f'{dir_path}/analyze/testpoints*')
+        all_train_csv = glob.glob(f'{dir_path}/analyze/trainpoints*')
+        all_test_csv.sort()
+        all_train_csv.sort()
+        for i, (test_csv, train_csv) in enumerate(zip(all_test_csv, all_train_csv)):
+            epoch = i
+            df_test = pd.read_csv(test_csv)
+            df_train = pd.read_csv(train_csv)
+            df_test_new = pd.merge(df_test, base_df, left_on='index', right_on='structure_idx')
+            df_train_new = pd.merge(df_train, base_df, left_on='index', right_on='structure_idx')
+            try:
+                test_score = analyze_flow.get_score(df_test_new, type='test')
+                train_score = analyze_flow.get_score(df_train_new, type='train')
+                analyze_flow.write_score_csv(epoch, test_score, train_score)
+            except Exception as e:
+                continue
+    conduct_flow(dir_path)
+    # dirs = glob.glob('/Users/y1u0d2/Desktop/Lab/result/nnp-train/two-body/for_angular/nnp*')
+    # for directory in dirs:
+    #     conduct_flow(directory)
--- scripts/n2p2_train.py
+++ scripts/n2p2_train.py
@@ -1,27 +1,30 @@
 import copy
 
-from n2p2 import n2p2_training
+from n2p2.n2p2_training import n2p2TrainingFlow
 
-obj = n2p2_training.n2p2TrainingFlow('/home/y1u0d2/Program/n2p2/two-body')
-
-pair_min = 3
-pair_max = 20
-
-r_cut_min = 5
-r_cut_max = 15
-
-r_cut = r_cut_min
-num_pairs = pair_min
-
-while r_cut <= r_cut_max:
-    while num_pairs <= pair_max:
-        cp_obj = copy.copy(obj)
-        cp_obj.set_up(f'nnp-train_{r_cut}_{num_pairs}')
-        cp_obj.set_up_sf(r_cut, num_pairs, f'nnp-train_{r_cut}_{num_pairs}')
-        cp_obj.run_n2p2(f'nnp-train_{r_cut}_{num_pairs}')
-        num_pairs += 1
-    num_pairs = pair_min
-    r_cut += 1
+# [r_cut, num_pairs]
+params = [
+    [5, 3],
+    [5, 6],
+    [5, 9],
+    [5, 5],
+    [5, 10],
+    [5, 15],
+    [10, 5],
+    [10, 10],
+    [10, 15],
+    [15, 5],
+    [15, 10],
+    [15, 15],
+]
+obj = n2p2TrainingFlow('/home/y1u0d2/Program/n2p2/two-body')
+for param in params:
+    r_cut = param[0]
+    num_pairs = param[1]
+    cp_obj = copy.copy(obj)
+    cp_obj.set_up(f'nnp-train_{r_cut}_{num_pairs}')
+    cp_obj.set_up_sf(r_cut, num_pairs, f'nnp-train_{r_cut}_{num_pairs}')
+    cp_obj.run_n2p2(f'nnp-train_{r_cut}_{num_pairs}')
 
 
 
