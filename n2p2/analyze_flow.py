81c49011360ad5fa7a60c65927694f8fa1877700
y1u0d2a1i <y1u0d2a1i@icloud.com>
2021-11-27 15:16:48 +0900

    force r2 output

--- n2p2/analyze_flow.py
+++ n2p2/analyze_flow.py
@@ -23,34 +23,45 @@ class N2p2AnalyzeFlow():
         else:
             os.mkdir(path)
 
-    def convert_test_train_csv(self):
-        test_files = glob.glob(f'{self.dir_path}/testpoints.0000*')
-        train_files = glob.glob(f'{self.dir_path}/trainpoints.0000*')
+    def convert_test_train_csv(self, type=1):
+        """
+        convert n2p2 output to csv
+        :param type: {1:energy, 2:force}
+        :return:
+        """
+        if type == 1:
+            header_num = 10
+            test_files = glob.glob(f'{self.dir_path}/testpoints.0000*')
+            train_files = glob.glob(f'{self.dir_path}/trainpoints.0000*')
+        elif type == 2:
+            header_num = 11
+            test_files = glob.glob(f'{self.dir_path}/testforces.0000*')
+            train_files = glob.glob(f'{self.dir_path}/trainforces.0000*')
         if test_files and train_files:
             obj = ConvertCsv(output_dir=f'{self.dir_path}/analyze', target_dir=self.dir_path)
             for test_file_path, train_file_path in zip(test_files, train_files):
                 out_test_file_name = test_file_path.split('/')[-1]
                 out_train_file_name = train_file_path.split('/')[-1]
                 # csvファイルに変換
-                obj.convert_n2p2_output_to_csv(10, out_test_file_name, f'{out_test_file_name}')
-                obj.convert_n2p2_output_to_csv(10, out_train_file_name, f'{out_train_file_name}')
+                obj.convert_n2p2_output_to_csv(header_num, out_test_file_name, f'{out_test_file_name}')
+                obj.convert_n2p2_output_to_csv(header_num, out_train_file_name, f'{out_train_file_name}')
 
     @staticmethod
-    def get_score(df, type):
+    def get_score(df, process, type='E'):
         try:
-            mae = mean_absolute_error(df['Eref'], df['Ennp'])
-            mse = mean_squared_error(df['Eref'], df['Ennp'])
+            mae = mean_absolute_error(df[f'{type}ref'], df[f'{type}nnp'])
+            mse = mean_squared_error(df[f'{type}ref'], df[f'{type}nnp'])
             rmse = np.sqrt(mse)
-            r2 = r2_score(df['Eref'], df['Ennp'])
+            r2 = r2_score(df[f'{type}ref'], df[f'{type}nnp'])
         except TypeError as e:
             print('NANが含まれています')
             return
-        return [type, mae, mse, rmse, r2]
+        return [process, mae, mse, rmse, r2]
 
-    def write_score_csv(self, epoch, test_score, train_score):
+    def write_score_csv(self, epoch, test_score, train_score, type='E'):
         test_score.insert(0, epoch)
         train_score.insert(0, epoch)
-        with open(f'{self.dir_path}/analyze/score.csv', 'a') as f:
+        with open(f'{self.dir_path}/analyze/score_{type}.csv', 'a') as f:
             writer = csv.writer(f)
             if epoch == 0:
                 writer.writerow(['epoch', 'type', 'MAE', 'MSE', 'RMSE', 'R2'])
@@ -62,7 +73,9 @@ if __name__ == '__main__':
     def conduct_flow(dir_path, is_send_local=False, scp_dir_path=None):
         analyze_flow = N2p2AnalyzeFlow(dir_path)
         analyze_flow.make_analyze_dir()
-        analyze_flow.convert_test_train_csv()
+        analyze_flow.convert_test_train_csv(type=1)
+        analyze_flow.convert_test_train_csv(type=2)
+        # Energy
         base_df = get_reindex_base(is_gpu=False)
         all_test_csv = glob.glob(f'{dir_path}/analyze/testpoints*')
         all_train_csv = glob.glob(f'{dir_path}/analyze/trainpoints*')
@@ -75,9 +88,24 @@ if __name__ == '__main__':
             df_test_new = pd.merge(df_test, base_df, left_on='index', right_on='structure_idx')
             df_train_new = pd.merge(df_train, base_df, left_on='index', right_on='structure_idx')
             try:
-                test_score = analyze_flow.get_score(df_test_new, type='test')
-                train_score = analyze_flow.get_score(df_train_new, type='train')
-                analyze_flow.write_score_csv(epoch, test_score, train_score)
+                test_score = analyze_flow.get_score(df_test_new, process='test', type='E')
+                train_score = analyze_flow.get_score(df_train_new, process='train', type='E')
+                analyze_flow.write_score_csv(epoch, test_score, train_score, type='E')
+            except Exception as e:
+                continue
+        # Force
+        all_test_csv = glob.glob(f'{dir_path}/analyze/testforces*')
+        all_train_csv = glob.glob(f'{dir_path}/analyze/trainforces*')
+        all_test_csv.sort()
+        all_train_csv.sort()
+        for i, (test_csv, train_csv) in enumerate(zip(all_test_csv, all_train_csv)):
+            epoch = i
+            df_test = pd.read_csv(test_csv)
+            df_train = pd.read_csv(train_csv)
+            try:
+                test_score = analyze_flow.get_score(df_test, process='test', type='F')
+                train_score = analyze_flow.get_score(df_train, process='train', type='F')
+                analyze_flow.write_score_csv(epoch, test_score, train_score, type='F')
             except Exception as e:
                 continue
         if is_send_local and scp_dir_path is not None:
@@ -86,6 +114,6 @@ if __name__ == '__main__':
                 os.mkdir(f'{scp_dir_path}/{dir_name}')
             shutil.move(f'{dir_path}/analyze', f'{scp_dir_path}/{dir_name}/')
 
-    dirs = glob.glob('/Users/y1u0d2/Desktop/Lab/result/nnp-train/rdf-inform/20211117/angular/01')
+    dirs = glob.glob('/Users/y1u0d2/desktop/Lab/result/nnp-train/20211126/nnp-train_10_10')
     for directory in dirs:
         conduct_flow(directory)
