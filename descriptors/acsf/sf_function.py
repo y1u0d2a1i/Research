10097e9e177767d6e730c32f8269e5b46a0ccae1
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-07-31 10:58:20 +0900

    no more developed

--- descriptors/acsf/sf_function.py
+++ descriptors/acsf/sf_function.py
@@ -20,3 +20,8 @@ class SymmetryFunction:
         if cut_func_type == 1:
            cutoff_func = self.cutoff_func1(r_ij, r_cutoff)
         return math.exp(-1 * eta * (r_ij - r_shift) ** 2) * cutoff_func
+    
+    def ang_symmetry_function_3(theta, lambdas, zeta, is_degree=True):
+        if is_degree:
+            theta = np.radians(theta)
+        return 2**(1-zeta) * (1 + lambdas * np.cos(theta))**zeta
